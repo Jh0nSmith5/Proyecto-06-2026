db87bf15fdd487267682bd81de6ccc02feaca21e
Victor <magnetitafe@gmail.com>
2026-06-27 19:21:57 -0700

    victor - creacion cajero.dart

---
+++ cajero.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+class CuentaBancaria {
+    Map<int, List<dynamic>> inventario = {
+        12345: ['Jhonat', 1500],
+        23456: ['Jonathan', 2000],
+        34567: ['Cesar', 1575],
+        45678: ['Victor', 1200],
+    };
+}
